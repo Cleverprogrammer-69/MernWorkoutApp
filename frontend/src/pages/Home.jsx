fb05bd0e7870053ec8189ebcd7b2babf9c661727
Cleverprogrammer-69 <76812283+Cleverprogrammer-69@users.noreply.github.com>
2023-10-26 18:22:44 +0500

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
     useEffect(() => {
       const fetchWorkouts = async () => {
         try {
-          const response = await axios.get(`/api/workouts`);
+          const response = await axios.get(`https://mern-workout-api.vercel.app/api/workouts`);
 
           if (response.status !== 200) {
             throw new Error('Network response was not ok');
