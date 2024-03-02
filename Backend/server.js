cb20a355ddb9f555f40d73aaa2e44f1b66792ac5
Cleverprogrammer-69 <hafizabdullah376@gmail.com>
2024-03-02 16:18:32 +0500

    some server changes

--- Backend/server.js
+++ Backend/server.js
@@ -1,31 +1,22 @@
 import express from "express";
 import router from "./routes/WorkoutsRoute.js";
 import dotenv from "dotenv";
-import mongoose from 'mongoose'
-import cors from "cors"
-import http from "http"
-// import cookieParser from "cookie-parser";
-const app=express()
-const PORT=3002
+import mongoose from "mongoose";
+import cors from "cors";
+import http from "http";
+const app = express();
+const PORT = process.env.PORT || 3002;
 
-app.use(express.json())
-app.use(cors(
-    {
-        origin: ["https://mern-workout-frontend.vercel.app"],
-        methods: ["POST", "GET", "PATCH", "DELETE"],
-        credentials: true
-    }
-));
+app.use(express.json());
+app.use(cors("*"));
 app.use(express.urlencoded({ extended: false }));
 
-app.use((req,res,next)=>{
-    console.log(req.path,req.method)
-    next()
-})
-mongoose.connect(process.env.MONGO_URL)
-app.use("/api/workouts",router)
-app.listen(PORT,()=>{
-    console.log("Server is Running")
-})
-
-
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+mongoose.connect(process.env.MONGO_URL);
+app.use("/api/workouts", router);
+app.listen(PORT, () => {
+  console.log("Server is Running");
+});
